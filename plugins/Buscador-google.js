ec3ad7bc1687b1537916fd0252a8e9485ac8eb68
Rufino ✝️ <rufinofelipe495@gmail.com>
2025-12-08 22:44:07 +0100

    Update error message and command usage text
    Signed-off-by: Rufino ✝️ <rufinofelipe495@gmail.com>

--- plugins/Buscador-google.js
+++ plugins/Buscador-google.js
@@ -1,7 +1,7 @@
 import translate from '@vitalets/google-translate-api';
 import fetch from 'node-fetch';
 const handler = async (m, {args, usedPrefix, command}) => {
-  const msg = `👑 *Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos en:*\n*- https://cloud.google.com/translate/docs/languages*`;
+  const msg = `🌸 *Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos en:*\n*- https://cloud.google.com/translate/docs/languages*`;
   if (!args || !args[0]) return m.reply(msg);
   let lang = args[0];
   let text = args.slice(1).join(' ');
@@ -33,7 +33,7 @@ const handler = async (m, {args, usedPrefix, command}) => {
       const result2 = loll.result.translated;
       await m.reply('*Traducción:* ' + result2);
     } catch {
-      await m.reply('✨️ *Ocurrió Un Error*');
+      await m.reply('❌ *Ocurrió Un Error*');
     }
   }
 };
